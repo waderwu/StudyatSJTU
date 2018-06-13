9fef5727b9da13e10595ee4e3cb2f99d59d2edf6
Hakase <phyank@users.noreply.github.com>
2018-06-13 22:16:25 +0800

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   frontend/css/infostyle.css 	modified:   frontend/index.html 	modified:   frontend/js/app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -50,9 +50,9 @@ angular.module("studyAtSjtu",['chart.js']).service('MainService',[MainService])
 
         self.popout_rs="pprs";
 
-    self.classes=[{num:1,name:"数据结构",tasks:{task1:{num:1,comment:"实验1很长很长很长很长很长很长123123123",complete:false,release:"2/14",ddl:"3/14",detail:"####任务详情\n    如此这般\n    如此这般",outdate:false},task2:{num:2,comment:"实验2",complete:true,release:"2/14",ddl:"3/14",detail:"##任务详情\n    如此这般",outdate:false},task3:{num:3,comment:"实验3",complete:true,release:"2/14",ddl:"3/14",detail:"##任务详情\n    如此这般",outdate:true}},info:"上海交通大学是我国历史"},
-        {numid:2,name:"计算机网",tasks:{task1:{num:1,comment:"实验1很长很长很长很长很长很长123123123",complete:false,release:"2/14",ddl:"3/14",detail:"##任务详情\n    如此这般",outdate:false},task2:{num:2,comment:"实验2",complete:true,release:"2/14",ddl:"3/14",detail:"##任务详情\n    如此这般",outdate:false},task3:{num:3,comment:"实验3",complete:false,release:"2/14",ddl:"3/14",detail:"##任务详情\n    如此这般",outdate:true}},info:"上海交通大学是我国历史最悠久、享誉海内外的著名高等学府之一，是教育部直属并与上海市共建的全国重点大学。经过122年的不懈努力，上海交通大学已经成为一所“综合性、研究型、国际化”的国内一流、国际知名大学。 "},
-        {num:3,name:"编译原理",tasks:{task1:{num:1,comment:"实验1很长很长很长很长很长很长123123123",complete:false,release:"2/14",ddl:"3/14",detail:"##任务详情\n    如此这般",outdate:false},task2:{num:2,comment:"实验2",complete:true,release:"2/14",ddl:"3/14",detail:"##任务详情\n    如此这般",outdate:false},task3:{num:3,comment:"实验3",complete:false,release:"2/14",ddl:"3/14",detail:"##任务详情\n    如此这般",outdate:true}},info:"上海交通大学是我国历史最悠久、享誉海内外的著名高等学府之一，是教育部直属并与上海市共建的全国重点大学。经过122年的不懈努力，上海交通大学已经成为一所“综合性、研究型、国际化”的国内一流、国际知名大学。 "},
+    self.classes=[{num:1,name:"数据结构",tasks:{task1:{num:1,comment:"实验1很长很长很长很长很长很长123123123",complete:false,release:"2/14",ddl:"3/14",detail:"####任务详情\n\n    如此这般\n    如此这般",outdate:false},task2:{num:2,comment:"实验2",complete:true,release:"2/14",ddl:"3/14",detail:"##任务详情\n    如此这般",outdate:false},task3:{num:3,comment:"实验3",complete:true,release:"2/14",ddl:"3/14",detail:"##任务详情\n    如此这般",outdate:true}},info:"上海交通大学是我国历史"},
+        {numid:2,name:"计算机网",tasks:{task1:{num:1,comment:"实验1很长很长很长很长很长很长123123123",complete:false,release:"2/14",ddl:"3/14",detail:"##任务详情\n\n    如此这般",outdate:false},task2:{num:2,comment:"实验2",complete:true,release:"2/14",ddl:"3/14",detail:"##任务详情\n    如此这般",outdate:false},task3:{num:3,comment:"实验3",complete:false,release:"2/14",ddl:"3/14",detail:"##任务详情\n    如此这般",outdate:true}},info:"上海交通大学是我国历史最悠久、享誉海内外的著名高等学府之一，是教育部直属并与上海市共建的全国重点大学。经过122年的不懈努力，上海交通大学已经成为一所“综合性、研究型、国际化”的国内一流、国际知名大学。 "},
+        {num:3,name:"编译原理",tasks:{task1:{num:1,comment:"实验1很长很长很长很长很长很长123123123",complete:false,release:"2/14",ddl:"3/14",detail:"##任务详情\n\n    如此这般",outdate:false},task2:{num:2,comment:"实验2",complete:true,release:"2/14",ddl:"3/14",detail:"##任务详情\n    如此这般",outdate:false},task3:{num:3,comment:"实验3",complete:false,release:"2/14",ddl:"3/14",detail:"##任务详情\n    如此这般",outdate:true}},info:"上海交通大学是我国历史最悠久、享誉海内外的著名高等学府之一，是教育部直属并与上海市共建的全国重点大学。经过122年的不懈努力，上海交通大学已经成为一所“综合性、研究型、国际化”的国内一流、国际知名大学。 "},
         {num:4,name:"Windows",tasks:{task1:{num:1,comment:"实验1很长很长很长很长很长很长123123123",complete:false,release:"2/14",ddl:"3/14",detail:"##任务详情\n    如此这般",outdate:false},task2:{num:2,comment:"实验2",complete:true,release:"2/14",ddl:"3/14",detail:"##任务详情\n    如此这般",outdate:false},task3:{num:3,comment:"实验3",complete:false,release:"2/14",ddl:"3/14",detail:"##任务详情\n    如此这般",outdate:true}},info:"上海交通大学是我国历史最悠久、享誉海内外的著名高等学府之一，是教育部直属并与上海市共建的全国重点大学。经过122年的不懈努力，上海交通大学已经成为一所“综合性、研究型、国际化”的国内一流、国际知名大学。 "}
         ];
 
@@ -160,15 +160,28 @@ angular.module("studyAtSjtu",['chart.js']).service('MainService',[MainService])
         }
     };
 
+        self.compare_by_overall_score=function(a,b){
+            if(a[0]<b[0]) return 1;
+            else if (a[0]>b[0]) return -1;
+            else return 0
+        };
+
+        self.compare_by_test_score=function(a,b){
+            if(a[1]<b[1]) return 1;
+            else if (a[1]>b[1]) return -1;
+            else return 0
+        };
+
     self.view_single=function (num,from) {
         self.current_single_num=num;
         self.prev_tab=from;
-        self.current_single={num:5,name:"操作系统-薛质-2018",year:2018,teacher:"薛质",resources:[{name:"习题答案3",url:"#",detail:"    答案3"},{name:"习题答案2",url:"#",detail:"    答案2"},{name:"习题答案1",url:"#",detail:"    答案1"}],tasks:{task1:{num:1,comment:"实验1很长很长很长很长很长很长123123123",complete:false,release:"2/14",ddl:"3/14",detail:"##任务详情\n    如此这般",outdate:false},task2:{num:2,comment:"实验2",complete:true,release:"2/14",ddl:"3/14",detail:"##任务详情\n    如此这般",outdate:false},task3:{num:3,comment:"实验3",complete:true,release:"2/14",ddl:"3/14",detail:"##任务详情\n    如此这般",outdate:true}},finish:true,rating:4,score:85,people:55,failrate:"3%",scorelist:[70,70,70,70,80,80,81],history_average:[70,71,70,69,67,72]};
+        self.current_single={num:5,name:"操作系统-薛质-2018",year:2018,teacher:"薛质",resources:[{name:"习题答案3",url:"#",detail:"    答案3"},{name:"习题答案2",url:"#",detail:"    答案2"},{name:"习题答案1",url:"#",detail:"    答案1"}],tasks:{task1:{num:1,comment:"实验1很长很长很长很长很长很长123123123",complete:false,release:"2/14",ddl:"3/14",detail:"##任务详情\n    如此这般",outdate:false},task2:{num:2,comment:"实验2",complete:true,release:"2/14",ddl:"3/14",detail:"##任务详情\n    如此这般",outdate:false},task3:{num:3,comment:"实验3",complete:true,release:"2/14",ddl:"3/14",detail:"##任务详情\n    如此这般",outdate:true}},finish:true,rating:4,score:85,people:55,failrate:"3%",scorelist:[[70,80,30],[70,80,30],[71,80,30],[60,70,30],[85,83,50],[80,40,20],[88,66,55]],history_average:[70,71,70,69,67,72]};
+        self.current_single.scorelist.sort(self.compare_by_overall_score);
         MainService.change_content(MainService.content_single);
     };
 
     self.comment_form_user="anonymous";
-    self.comment_form_ranking=5;
+    self.comment_form_ranking=1;
     self.comment_form_text="none";
 
     self.get_nav_class=function(index){
@@ -200,7 +213,10 @@ angular.module("studyAtSjtu",['chart.js']).service('MainService',[MainService])
 
     self.get_trusted_html=function(text){
         return $sce.trustAsHtml(self.converter.makeHtml(text));
-    }
+
+
+    };
+
 
 
 }]).controller("SearchCtl",["MainService",function (MainService) {
